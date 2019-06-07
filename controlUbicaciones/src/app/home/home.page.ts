b5df2b84eba1849f2278b9388b4bf562daaa475a
tomas41730 <43442649+tomas41730@users.noreply.github.com>
2019-06-07 00:17:57 -0400

    Agregando mapa

--- controlUbicaciones/src/app/home/home.page.ts
+++ controlUbicaciones/src/app/home/home.page.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+import { Plugins, GeolocationPluginWeb } from '@capacitor/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire';
+const { Geolocation } = Plugins;
+
+declare var google;
 
 @Component({
   selector: 'app-home',
@@ -6,7 +15,47 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  locations: Observable<any>;
+  locationCollection: AngularFirestoreCollection<any>
+  user = null;
 
-  constructor() {}
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  markers = [];
 
+  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore) {
+    this.anonLogin();
+  }
+
+  ionViewWillEnter(){
+    this.loadMap();
+  }
+
+  loadMap(){
+    let latLng = new google.maps.LatLng(51.9036442, 7.6673267);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 5,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  }
+
+  anonLogin(){
+    this.afAuth.auth.signInAnonymously().then(user => {
+      console.log(user);
+      this.user = user;
+
+      this.locationCollection = this.afs.collection(
+        `locations/$(this.user.uid}/track`,
+        ref => ref.orderBy('timestamp')
+      );
+
+      //Cargando datos de firebase
+
+      //Actualizando mapa
+    })
+  }
 }
--- controlUbicaciones/src/environments/environment.ts
+++ controlUbicaciones/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDelLn2boUPz7jGQOaDbKekRQBuTewpsoI",
+    authDomain: "chachas-70327.firebaseapp.com",
+    databaseURL: "https://chachas-70327.firebaseio.com",
+    projectId: "chachas-70327",
+    storageBucket: "chachas-70327.appspot.com",
+    messagingSenderId: "762124770443",
+    appId: "1:762124770443:web:db3dc8e275a44850"
+  }
 };
 
 /*
