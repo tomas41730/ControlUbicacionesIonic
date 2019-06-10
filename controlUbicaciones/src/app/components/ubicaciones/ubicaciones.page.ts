56e374db92a4de1dc6ed0d4e63c4a3b7c2432acc
tomas41730 <43442649+tomas41730@users.noreply.github.com>
2019-06-10 09:58:42 -0400

    Instanciando datos al modelo requerido

--- controlUbicaciones/src/app/components/ubicaciones/ubicaciones.page.ts
+++ controlUbicaciones/src/app/components/ubicaciones/ubicaciones.page.ts
@@ -1,15 +1,73 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
+declare var google;
 @Component({
   selector: 'app-ubicaciones',
   templateUrl: './ubicaciones.page.html',
   styleUrls: ['./ubicaciones.page.scss'],
 })
-export class UbicacionesPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class UbicacionesPage{
+  ubicaciones: Observable<any>;
+  listaUbicaciones: AngularFirestoreCollection<any>
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  markers = [];
+  constructor(private afs: AngularFirestore) { 
+    this.anonLogin();
   }
+  ionViewWillEnter(){
+    this.loadMap();
+  }
+  loadMap(){
+    let latLng = new google.maps.LatLng(-17.380494, -66.179610);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  }
+
+  anonLogin(){
+   
+      this.listaUbicaciones = this.afs.collection('UbicacionMoto');
+
+      //Cargando datos de firebase
+        this.ubicaciones = this.listaUbicaciones.snapshotChanges().pipe(
+          map(actions => 
+            actions.map(a => {
+              const data = a.payload.doc.data();
+              const id =  a.payload.doc.id;
+              return { id, ...data };
+            })
+          )
+        );
+        
+      //Actualizando mapa
+      this.ubicaciones.subscribe(ubicaciones =>
+        {
+         console.log('new ubicaciones: ', ubicaciones);
+         this.updateMap(ubicaciones); 
+        })
+  }
+  updateMap(ubicaciones){
+    this.markers.map(marker => marker.setMap(null));
+    this.markers = [];
+
+    for (let loc of ubicaciones){
+      let latLng = new google.maps.LatLng(loc.lat, loc.lng);
+
+      let marker = new google.maps.Marker({
+        position: latLng,
+        animation: google.maps.Animation.Drop,
+        map: this.map
+      });
+      this.markers.push(marker);
+    }
+  } 
 
 }
