13bbc107ce5323851a37ce844bf9c9efcec65434
tomas41730 <43442649+tomas41730@users.noreply.github.com>
2019-06-10 14:01:50 -0400

    Agregando lables a los marcadores

--- controlUbicaciones/src/app/home/home.page.ts
+++ controlUbicaciones/src/app/home/home.page.ts
@@ -1,17 +1,17 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 
-import { Plugins, GeolocationPluginWeb } from '@capacitor/core';
-import { AngularFireAuth } from '@angular/fire/auth';
+//import { Plugins, GeolocationPluginWeb } from '@capacitor/core';
+//import { AngularFireAuth } from '@angular/fire/auth';
 import { Observable } from 'rxjs';
 import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
-import { AngularFireModule } from '@angular/fire';
-import { Router } from '@angular/router';
+//import { AngularFireModule } from '@angular/fire';
+//import { Router } from '@angular/router';
 
-const { Geolocation } = Plugins;
+//const { Geolocation } = Plugins;
 
 declare var google;
 import { map } from 'rxjs/operators';
-import { animationFrame } from 'rxjs/internal/scheduler/animationFrame';
+//import { animationFrame } from 'rxjs/internal/scheduler/animationFrame';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -69,16 +69,30 @@ export class HomePage {
 
     for (let loc of ubicaciones){
       if(loc.disponible){
-      let latLng = new google.maps.LatLng(loc.latitud, loc.longitud);
-
+         let latLng = new google.maps.LatLng(loc.latitud, loc.longitud);   
       let marker = new google.maps.Marker({
         position: latLng,
         animation: google.maps.Animation.Drop,
         icon: { url: "http://maps.google.com/mapfiles/ms/icons/green-dot.png" },
+        label: {text: loc.nombreMotoTaxi+" "+ loc.apellidoMotoTaxi, color: "white"},
+        title: loc.nombreMotoTaxi,
         map: this.map
       });
       this.markers.push(marker);
-    }
+      }
+     else{
+      let latLng = new google.maps.LatLng(loc.latitud, loc.longitud);   
+      let marker = new google.maps.Marker({
+        position: latLng,
+        animation: google.maps.Animation.Drop,
+        icon: { url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png" },
+        label: {text: loc.nombreMotoTaxi+" "+ loc.apellidoMotoTaxi, color: "white"},
+        title: loc.nombreMotoTaxi,
+        map: this.map
+      });
+      this.markers.push(marker);
+     }
+    
    
     }
   } 
