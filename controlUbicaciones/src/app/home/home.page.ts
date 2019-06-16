e4f9192a1384d2b7980cba6fba1fc36357c9fe65
tomas41730 <43442649+tomas41730@users.noreply.github.com>
2019-06-16 19:50:22 -0400

    Agrgando metodo para devolver moto mas cerca a la sucursal

--- controlUbicaciones/src/app/home/home.page.ts
+++ controlUbicaciones/src/app/home/home.page.ts
@@ -19,6 +19,7 @@ export class HomePage {
   markers = [];
   constructor(private afs: AngularFirestore) { 
     this.anonLogin();
+    
   }
   ionViewWillEnter(){
     this.loadMap();
@@ -36,7 +37,7 @@ export class HomePage {
   }
 
   anonLogin(){
-   
+    
       this.listaUbicaciones = this.afs.collection('motoTaxis');
 
       //Cargando datos de firebase
@@ -55,17 +56,18 @@ export class HomePage {
         {
          console.log('ubicaciones de los conductores: ', ubicaciones);
          this.updateMap(ubicaciones); 
+         console.log(this.asignacionMoto(ubicaciones));
         })
   }
   updateMap(ubicaciones){
     this.markers.map(marker => marker.setMap(null));
     this.markers = [];
-
     for (let loc of ubicaciones){
+      
       if(loc.disponible){
         this.estaDisponible = true;
-         let latLng = new google.maps.LatLng(loc.latitud, loc.longitud);   
-      let marker = new google.maps.Marker({
+        let latLng = new google.maps.LatLng(loc.latitud, loc.longitud);         
+        let marker = new google.maps.Marker({
         position: latLng,
         animation: google.maps.Animation.Drop,
         icon: { url: "http://maps.google.com/mapfiles/ms/icons/green-dot.png" },
@@ -92,5 +94,74 @@ export class HomePage {
    
     }
   } 
+  displayRoute(origin, destination, service, display) {
+    service.route({
+      origin: origin,
+      destination: destination,
+      //waypoints: [{location: 'Adelaide, SA'}, {location: 'Broken Hill, NSW'}],
+      travelMode: 'DRIVING',
+      avoidTolls: true
+    }, function(response, status) {
+      if (status === 'OK') {
+        display.setDirections(response);
+      } else {
+        alert('Could not display directions due to: ' + status);
+      }
+    });
+  }
+  // computeTotalDistance(result) {
+  //   var total = 0;
+  //   var myroute = result.routes[0];
+  //   for (var i = 0; i < myroute.legs.length; i++) {
+  //     total += myroute.legs[i].distance.value;
+  //   }
+  //   total = total / 1000;
+  //   document.getElementById('total').innerHTML = total + ' km';
+  // }
+  agregandoRuta(){
+    let origin = new google.maps.LatLng(-17.3892211,-66.2162773);  
+    let destiny = new google.maps.LatLng(-17.3868231,-66.2111644); 
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer({
+      draggable: true,
+      map: this.map,
+      panel: document.getElementById('right-panel')
+    });
 
+    directionsDisplay.addListener('directions_changed', function() {
+   //this.computeTotalDistance(directionsDisplay.getDirections());
+    });
+
+    this.displayRoute(origin, destiny, directionsService,
+        directionsDisplay);
+  }
+  asignacionMoto(ubicaciones){
+    let motosDistancias = [];
+    let sucursal = new google.maps.LatLng(-17.3921318,-66.2234896);
+    for (let loc of ubicaciones){
+      
+      if(loc.latitud != null){ 
+        let latLng = new google.maps.LatLng(loc.latitud, loc.longitud); 
+        let total = google.maps.geometry.spherical.computeDistanceBetween(latLng, sucursal); 
+        console.log('La distancia del conductor '+loc.apellidoMotoTaxi + ' es '+ total + ' metros');
+        motosDistancias.push(total);
+      }   
+    } 
+    var moto = null;
+    var min=Math.min.apply(null, motosDistancias);
+    for (let loc of ubicaciones){
+      
+      if(loc.latitud != null){
+        this.estaDisponible = true;
+        let latLng = new google.maps.LatLng(loc.latitud, loc.longitud); 
+        let total = google.maps.geometry.spherical.computeDistanceBetween(latLng, sucursal);
+        if( total === min){
+          moto = loc.nombreMotoTaxi+" "+loc.apellidoMotoTaxi;
+        }
+      }   
+    } 
+    
+    console.log('La menor distancia es '+ min+' de '+ moto);     
+    return moto;
+  } 
 }
